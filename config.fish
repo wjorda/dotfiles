08c089d4fb1780595ab61a5ca2a11d95ddaaec69
wesj <wes@wesj.org>
2021-11-28 13:21:22 -0500

    downgrade ndk to r22

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 set -x JAVA_HOME /usr/local/Cellar/openjdk/17.0.1
 set -x ANDROID_HOME ~/Library/Android/sdk
-set -x NDK_HOME $ANDROID_HOME/ndk/23.1.7779620
+set -x NDK_HOME $ANDROID_HOME/ndk/22.1.7171670
 
 # android sdk
 set -x PATH $PATH $ANDROID_HOME/cmdline-tools/latest/bin $ANDROID_HOME/sdk/platform-tools/
