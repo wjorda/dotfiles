53b68821febe2e2096caff8b6d15502a2dd8532b
wesj <wes@wesj.org>
2021-11-28 11:41:36 -0500

    switch from rust to rustup

--- config.fish
+++ config.fish
@@ -2,7 +2,11 @@ set -x JAVA_HOME /usr/local/Cellar/openjdk/17.0.1
 set -x ANDROID_HOME ~/Library/Android/sdk
 set -x NDK_HOME $ANDROID_HOME/ndk/23.1.7779620
 
+# android sdk
 set -x PATH $PATH $ANDROID_HOME/cmdline-tools/latest/bin $ANDROID_HOME/sdk/platform-tools/
+
+# rustup
+set -x PATH $PATH $HOME/.cargo/bin
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
