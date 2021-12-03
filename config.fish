402d9e343b89637ead6b78f1b0ecf425d00d323c
wesj <wes@wesj.org>
2021-12-03 13:21:29 -0500

    fix android path

--- config.fish
+++ config.fish
@@ -3,7 +3,7 @@ set -x ANDROID_HOME ~/Library/Android/sdk
 set -x NDK_HOME $ANDROID_HOME/ndk/22.1.7171670
 
 # android sdk
-set -x PATH $PATH $ANDROID_HOME/cmdline-tools/latest/bin $ANDROID_HOME/sdk/platform-tools/
+set -x PATH $PATH $ANDROID_HOME/cmdline-tools/latest/bin $ANDROID_HOME/platform-tools/
 
 # rustup
 set -x PATH $PATH $HOME/.cargo/bin
